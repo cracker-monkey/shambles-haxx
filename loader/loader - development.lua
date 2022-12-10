07156c0a24dd6d156833c9bb81c771c048807297
senpaioffice132 <senpaioffice30@gmail.com>
2022-12-10 16:46:38 +0200

    Names
    Changed the names of files.

--- loader/loader - development.lua
+++ loader/loader - development.lua
@@ -11,7 +11,7 @@ end
 printc("Hello developer to shambles haxx!")
 
 if PlaceId == 299659045 or PlaceId == 292439477 then
-    loadstring(readfile("shambles haxx/games/phantom-forces/development/core/pf.lua"))()
+    loadstring(readfile("shambles haxx/games/phantom-forces/development/core/source.lua"))()
 else
-    loadstring(readfile("shambles haxx/games/universal/development/core/universal.lua"))()
+    loadstring(readfile("shambles haxx/games/universal/development/core/source.lua"))()
 end
