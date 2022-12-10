f476eb05b69105494b10f7347ba648c0f8553c8d
senpaioffice132 <senpaioffice30@gmail.com>
2022-12-10 15:16:42 +0200

    Loader Fix
    Fixed the loader again smh.

--- loader/loader - development.lua
+++ loader/loader - development.lua
@@ -17,5 +17,5 @@ printc("Hello developer to shambles haxx!")
 if PlaceId == 299659045 or PlaceId == 292439477 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/FunnyManWrehas/shambles-haxx/main/games/phantom-forces/development/core/phantom-forces%20-%20development.lua?token=GHSAT0AAAAAAB2IVB74JBOYMMUWXADD333KY4UQMQQ"))
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/FunnyManWrehas/shambles-haxx/main/games/universal/development/core/universal%20-%20development.lua?token=GHSAT0AAAAAAB2IVB743SQGHGG7IRNS63WQY4UQM5A"))
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/FunnyManWrehas/shambles-haxx/main/games/universal/development/core/universal%20-%20development.lua?token=GHSAT0AAAAAAB2IVB756GJDDGDS7PZ7P7DSY4UQ3MQ"))
 end
