b03d5ae4f3ae021e2b6a5acf0273b5de3acd6062
cracker monkey <wreh2199@gmail.com>
2022-12-12 17:58:45 +0200

    Loader
    removed some things that are not used

--- loader/loader - development.lua
+++ loader/loader - development.lua
@@ -1,12 +1,5 @@
 local PlaceId = game.PlaceId
 
-local function printc(text)
-    if syn then
-        printconsole(text, 255, 255, 255)
-    else
-        warn(text)
-    end
-end
 
 local function execute(path, r)
     if r then
@@ -22,15 +15,11 @@ do
     end
 
     execute("shambles haxx/globals/information.lua")
-    --execute("shambles haxx/websockets/main.lua")
+    -- execute("shambles haxx/websockets/main.lua")
 
     getgenv().Library = execute("shambles haxx/libraries/UI/UI.lua", true)
     getgenv().ThemeManager = execute("shambles haxx/libraries/Managers/Theme Manager.lua", true)
     getgenv().SaveManager = execute("shambles haxx/libraries/Managers/Configuration Manager.lua", true)
-
-    local username = getgenv().username
-
-    printc("Welcome, ".. username .." to shambles haxx!")
 
     if PlaceId == 299659045 or PlaceId == 292439477 then
         execute("shambles haxx/games/phantom-forces/development/core/source.lua")
